504e1deb2b6663b8814beba247dd5b0dd4d76d2b
anekrasova210804 <a.nekrasova1@uniyar.ac.ru>
2024-05-18 14:55:46 +0300

    Добавьте файлы проекта.

---
+++ FractalsLab1Julia6/JuliaFractal.cs
@@ -0,0 +1,15 @@
+namespace FractalsLab1Julia6
+{
+    public partial class JuliaFractalUI : Form
+    {
+        public JuliaFractalUI()
+        {
+            InitializeComponent();
+        }
+
+        private void JuliaFractalUI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
