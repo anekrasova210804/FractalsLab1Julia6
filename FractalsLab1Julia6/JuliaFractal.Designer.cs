4eab42e5740d68cb9563e7a734ae719c060677d4
anekrasova210804 <a.nekrasova1@uniyar.ac.ru>
2024-05-20 21:48:32 +0300

    Added some stuff

--- FractalsLab1Julia6/JuliaFractal.Designer.cs
+++ FractalsLab1Julia6/JuliaFractal.Designer.cs
@@ -30,14 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JuliaFractalUI));
             tableLayout = new TableLayoutPanel();
-            cNumber = new Label();
+            cNumberLabel = new Label();
             iterationsLabel = new Label();
             radiusLabel = new Label();
             viewOffSetXLabel = new Label();
             viewOffsetYLabel = new Label();
             zoomLabel = new Label();
-            сСhoice = new TextBox();
-            radiusChoice = new TextBox();
+            RealСhoice = new TextBox();
             offsetXChoice = new NumericUpDown();
             offsetYChoice = new NumericUpDown();
             zoomChoice = new NumericUpDown();
@@ -46,33 +45,36 @@
             juliaLabel = new Label();
             saveButton = new Button();
             renderButton = new Button();
+            ImaginaryChoice = new TextBox();
+            radiusChoice = new NumericUpDown();
             tableLayout.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)offsetXChoice).BeginInit();
             ((System.ComponentModel.ISupportInitialize)offsetYChoice).BeginInit();
             ((System.ComponentModel.ISupportInitialize)zoomChoice).BeginInit();
             ((System.ComponentModel.ISupportInitialize)iterationsChoice).BeginInit();
             ((System.ComponentModel.ISupportInitialize)juliaPictureBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)radiusChoice).BeginInit();
             SuspendLayout();
             // 
             // tableLayout
             // 
             tableLayout.BackColor = Color.GhostWhite;
-            tableLayout.ColumnCount = 7;
+            tableLayout.ColumnCount = 8;
             tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 3.499327F));
             tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 68.0597F));
             tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 24F));
             tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 160F));
             tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 22F));
-            tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 161F));
-            tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayout.Controls.Add(cNumber, 3, 3);
+            tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 90F));
+            tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 89F));
+            tableLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 22F));
+            tableLayout.Controls.Add(cNumberLabel, 3, 3);
             tableLayout.Controls.Add(iterationsLabel, 3, 5);
             tableLayout.Controls.Add(radiusLabel, 3, 7);
             tableLayout.Controls.Add(viewOffSetXLabel, 3, 9);
             tableLayout.Controls.Add(viewOffsetYLabel, 3, 11);
             tableLayout.Controls.Add(zoomLabel, 3, 13);
-            tableLayout.Controls.Add(сСhoice, 5, 3);
-            tableLayout.Controls.Add(radiusChoice, 5, 7);
+            tableLayout.Controls.Add(RealСhoice, 5, 3);
             tableLayout.Controls.Add(offsetXChoice, 5, 9);
             tableLayout.Controls.Add(offsetYChoice, 5, 11);
             tableLayout.Controls.Add(zoomChoice, 5, 13);
@@ -81,6 +83,8 @@
             tableLayout.Controls.Add(juliaLabel, 1, 15);
             tableLayout.Controls.Add(saveButton, 3, 15);
             tableLayout.Controls.Add(renderButton, 5, 15);
+            tableLayout.Controls.Add(ImaginaryChoice, 6, 3);
+            tableLayout.Controls.Add(radiusChoice, 5, 7);
             tableLayout.Dock = DockStyle.Fill;
             tableLayout.Location = new Point(0, 0);
             tableLayout.Name = "tableLayout";
@@ -105,19 +109,19 @@
             tableLayout.Size = new Size(1216, 784);
             tableLayout.TabIndex = 0;
             // 
-            // cNumber
+            // cNumberLabel
             // 
-            cNumber.AutoSize = true;
-            cNumber.BackColor = Color.MediumPurple;
-            cNumber.Dock = DockStyle.Fill;
-            cNumber.Font = new Font("MS Gothic", 11.25F, FontStyle.Bold);
-            cNumber.ForeColor = Color.GhostWhite;
-            cNumber.Location = new Point(855, 31);
-            cNumber.Name = "cNumber";
-            cNumber.Size = new Size(154, 33);
-            cNumber.TabIndex = 0;
-            cNumber.Text = "C:";
-            cNumber.TextAlign = ContentAlignment.MiddleCenter;
+            cNumberLabel.AutoSize = true;
+            cNumberLabel.BackColor = Color.MediumPurple;
+            cNumberLabel.Dock = DockStyle.Fill;
+            cNumberLabel.Font = new Font("MS Gothic", 11.25F, FontStyle.Bold);
+            cNumberLabel.ForeColor = Color.GhostWhite;
+            cNumberLabel.Location = new Point(835, 31);
+            cNumberLabel.Name = "cNumberLabel";
+            cNumberLabel.Size = new Size(154, 33);
+            cNumberLabel.TabIndex = 0;
+            cNumberLabel.Text = "C:";
+            cNumberLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // iterationsLabel
             // 
@@ -126,7 +130,7 @@
             iterationsLabel.Dock = DockStyle.Fill;
             iterationsLabel.Font = new Font("MS Gothic", 11.25F, FontStyle.Bold);
             iterationsLabel.ForeColor = Color.GhostWhite;
-            iterationsLabel.Location = new Point(855, 79);
+            iterationsLabel.Location = new Point(835, 79);
             iterationsLabel.Name = "iterationsLabel";
             iterationsLabel.Size = new Size(154, 35);
             iterationsLabel.TabIndex = 1;
@@ -140,7 +144,7 @@
             radiusLabel.Dock = DockStyle.Fill;
             radiusLabel.Font = new Font("MS Gothic", 11.25F, FontStyle.Bold);
             radiusLabel.ForeColor = Color.GhostWhite;
-            radiusLabel.Location = new Point(855, 126);
+            radiusLabel.Location = new Point(835, 126);
             radiusLabel.Name = "radiusLabel";
             radiusLabel.Size = new Size(154, 38);
             radiusLabel.TabIndex = 2;
@@ -154,7 +158,7 @@
             viewOffSetXLabel.Dock = DockStyle.Fill;
             viewOffSetXLabel.Font = new Font("MS Gothic", 11.25F, FontStyle.Bold);
             viewOffSetXLabel.ForeColor = Color.GhostWhite;
-            viewOffSetXLabel.Location = new Point(855, 176);
+            viewOffSetXLabel.Location = new Point(835, 176);
             viewOffSetXLabel.Name = "viewOffSetXLabel";
             viewOffSetXLabel.Size = new Size(154, 36);
             viewOffSetXLabel.TabIndex = 3;
@@ -168,7 +172,7 @@
             viewOffsetYLabel.Dock = DockStyle.Fill;
             viewOffsetYLabel.Font = new Font("MS Gothic", 11.25F, FontStyle.Bold);
             viewOffsetYLabel.ForeColor = Color.GhostWhite;
-            viewOffsetYLabel.Location = new Point(855, 226);
+            viewOffsetYLabel.Location = new Point(835, 226);
             viewOffsetYLabel.Name = "viewOffsetYLabel";
             viewOffsetYLabel.Size = new Size(154, 36);
             viewOffsetYLabel.TabIndex = 4;
@@ -182,83 +186,77 @@
             zoomLabel.Dock = DockStyle.Fill;
             zoomLabel.Font = new Font("MS Gothic", 11.25F, FontStyle.Bold);
             zoomLabel.ForeColor = Color.GhostWhite;
-            zoomLabel.Location = new Point(855, 274);
+            zoomLabel.Location = new Point(835, 274);
             zoomLabel.Name = "zoomLabel";
             zoomLabel.Size = new Size(154, 36);
             zoomLabel.TabIndex = 5;
             zoomLabel.Text = "Zoom:";
             zoomLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // сСhoice
+            // RealСhoice
             // 
-            сСhoice.BorderStyle = BorderStyle.FixedSingle;
-            сСhoice.Cursor = Cursors.IBeam;
-            сСhoice.Dock = DockStyle.Left;
-            сСhoice.Font = new Font("MS Gothic", 9F);
-            сСhoice.Location = new Point(1037, 34);
-            сСhoice.Name = "сСhoice";
-            сСhoice.Size = new Size(138, 19);
-            сСhoice.TabIndex = 6;
-            // 
-            // radiusChoice
-            // 
-            radiusChoice.BorderStyle = BorderStyle.FixedSingle;
-            radiusChoice.Cursor = Cursors.IBeam;
-            radiusChoice.Dock = DockStyle.Left;
-            radiusChoice.Font = new Font("MS Gothic", 9F);
-            radiusChoice.Location = new Point(1037, 129);
-            radiusChoice.Name = "radiusChoice";
-            radiusChoice.Size = new Size(138, 19);
-            radiusChoice.TabIndex = 8;
+            RealСhoice.BorderStyle = BorderStyle.FixedSingle;
+            RealСhoice.Cursor = Cursors.IBeam;
+            RealСhoice.Dock = DockStyle.Left;
+            RealСhoice.Font = new Font("MS Gothic", 9F);
+            RealСhoice.Location = new Point(1017, 34);
+            RealСhoice.Name = "RealСhoice";
+            RealСhoice.Size = new Size(73, 19);
+            RealСhoice.TabIndex = 6;
+            RealСhoice.Text = "0,12";
             // 
             // offsetXChoice
             // 
             offsetXChoice.BorderStyle = BorderStyle.FixedSingle;
+            tableLayout.SetColumnSpan(offsetXChoice, 2);
             offsetXChoice.DecimalPlaces = 16;
             offsetXChoice.Dock = DockStyle.Left;
             offsetXChoice.Font = new Font("MS Gothic", 9F);
-            offsetXChoice.Location = new Point(1037, 179);
+            offsetXChoice.Location = new Point(1017, 179);
             offsetXChoice.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
             offsetXChoice.Minimum = new decimal(new int[] { 10, 0, 0, int.MinValue });
             offsetXChoice.Name = "offsetXChoice";
-            offsetXChoice.Size = new Size(139, 19);
+            offsetXChoice.Size = new Size(173, 19);
             offsetXChoice.TabIndex = 9;
             // 
             // offsetYChoice
             // 
             offsetYChoice.BorderStyle = BorderStyle.FixedSingle;
+            tableLayout.SetColumnSpan(offsetYChoice, 2);
             offsetYChoice.DecimalPlaces = 16;
             offsetYChoice.Dock = DockStyle.Left;
             offsetYChoice.Font = new Font("MS Gothic", 9F);
-            offsetYChoice.Location = new Point(1037, 229);
+            offsetYChoice.Location = new Point(1017, 229);
             offsetYChoice.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
             offsetYChoice.Minimum = new decimal(new int[] { 10, 0, 0, int.MinValue });
             offsetYChoice.Name = "offsetYChoice";
-            offsetYChoice.Size = new Size(139, 19);
+            offsetYChoice.Size = new Size(173, 19);
             offsetYChoice.TabIndex = 10;
             // 
             // zoomChoice
             // 
             zoomChoice.BorderStyle = BorderStyle.FixedSingle;
+            tableLayout.SetColumnSpan(zoomChoice, 2);
             zoomChoice.Dock = DockStyle.Left;
             zoomChoice.Font = new Font("MS Gothic", 9F);
-            zoomChoice.Location = new Point(1037, 277);
+            zoomChoice.Location = new Point(1017, 277);
             zoomChoice.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             zoomChoice.Name = "zoomChoice";
-            zoomChoice.Size = new Size(129, 19);
+            zoomChoice.Size = new Size(173, 19);
             zoomChoice.TabIndex = 11;
-            zoomChoice.Value = new decimal(new int[] { 4, 0, 0, 0 });
+            zoomChoice.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
             // iterationsChoice
             // 
             iterationsChoice.BorderStyle = BorderStyle.FixedSingle;
+            tableLayout.SetColumnSpan(iterationsChoice, 2);
             iterationsChoice.Dock = DockStyle.Left;
             iterationsChoice.Font = new Font("MS Gothic", 9F);
-            iterationsChoice.Location = new Point(1037, 82);
+            iterationsChoice.Location = new Point(1017, 82);
             iterationsChoice.Maximum = new decimal(new int[] { 1000000, 0, 0, 0 });
             iterationsChoice.Minimum = new decimal(new int[] { 10, 0, 0, 0 });
             iterationsChoice.Name = "iterationsChoice";
-            iterationsChoice.Size = new Size(139, 19);
+            iterationsChoice.Size = new Size(173, 19);
             iterationsChoice.TabIndex = 12;
             iterationsChoice.Value = new decimal(new int[] { 50, 0, 0, 0 });
             // 
@@ -267,10 +265,10 @@
             juliaPictureBox.BackColor = SystemColors.WindowText;
             juliaPictureBox.BackgroundImageLayout = ImageLayout.Zoom;
             juliaPictureBox.Dock = DockStyle.Fill;
-            juliaPictureBox.Location = new Point(43, 34);
+            juliaPictureBox.Location = new Point(42, 34);
             juliaPictureBox.Name = "juliaPictureBox";
             tableLayout.SetRowSpan(juliaPictureBox, 12);
-            juliaPictureBox.Size = new Size(782, 684);
+            juliaPictureBox.Size = new Size(763, 684);
             juliaPictureBox.TabIndex = 13;
             juliaPictureBox.TabStop = false;
             // 
@@ -281,9 +279,9 @@
             juliaLabel.Dock = DockStyle.Fill;
             juliaLabel.Font = new Font("MS Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
             juliaLabel.ForeColor = SystemColors.ButtonFace;
-            juliaLabel.Location = new Point(43, 721);
+            juliaLabel.Location = new Point(42, 721);
             juliaLabel.Name = "juliaLabel";
-            juliaLabel.Size = new Size(782, 43);
+            juliaLabel.Size = new Size(763, 43);
             juliaLabel.TabIndex = 14;
             juliaLabel.Text = "Julia Set f(z) = z⁵ + c";
             juliaLabel.TextAlign = ContentAlignment.MiddleCenter;
@@ -295,7 +293,7 @@
             saveButton.FlatStyle = FlatStyle.Flat;
             saveButton.Font = new Font("MS Gothic", 12F, FontStyle.Bold);
             saveButton.ForeColor = SystemColors.ButtonFace;
-            saveButton.Location = new Point(855, 724);
+            saveButton.Location = new Point(835, 724);
             saveButton.Name = "saveButton";
             saveButton.Size = new Size(154, 37);
             saveButton.TabIndex = 15;
@@ -305,16 +303,40 @@
             // renderButton
             // 
             renderButton.BackColor = Color.MediumOrchid;
+            tableLayout.SetColumnSpan(renderButton, 2);
             renderButton.Dock = DockStyle.Fill;
             renderButton.FlatStyle = FlatStyle.Flat;
             renderButton.Font = new Font("MS Gothic", 12F, FontStyle.Bold);
             renderButton.ForeColor = SystemColors.ButtonFace;
-            renderButton.Location = new Point(1037, 724);
+            renderButton.Location = new Point(1017, 724);
             renderButton.Name = "renderButton";
-            renderButton.Size = new Size(155, 37);
+            renderButton.Size = new Size(173, 37);
             renderButton.TabIndex = 16;
             renderButton.Text = "Render";
             renderButton.UseVisualStyleBackColor = false;
+            renderButton.Click += renderButton_Click;
+            // 
+            // ImaginaryChoice
+            // 
+            ImaginaryChoice.BorderStyle = BorderStyle.FixedSingle;
+            ImaginaryChoice.Dock = DockStyle.Right;
+            ImaginaryChoice.Font = new Font("MS Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ImaginaryChoice.Location = new Point(1114, 34);
+            ImaginaryChoice.Name = "ImaginaryChoice";
+            ImaginaryChoice.Size = new Size(76, 19);
+            ImaginaryChoice.TabIndex = 17;
+            ImaginaryChoice.Text = "-0,66";
+            // 
+            // radiusChoice
+            // 
+            radiusChoice.BorderStyle = BorderStyle.FixedSingle;
+            tableLayout.SetColumnSpan(radiusChoice, 2);
+            radiusChoice.Font = new Font("MS Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            radiusChoice.Location = new Point(1017, 129);
+            radiusChoice.Name = "radiusChoice";
+            radiusChoice.Size = new Size(173, 19);
+            radiusChoice.TabIndex = 18;
+            radiusChoice.Value = new decimal(new int[] { 4, 0, 0, 0 });
             // 
             // JuliaFractalUI
             // 
@@ -335,20 +357,20 @@
             ((System.ComponentModel.ISupportInitialize)zoomChoice).EndInit();
             ((System.ComponentModel.ISupportInitialize)iterationsChoice).EndInit();
             ((System.ComponentModel.ISupportInitialize)juliaPictureBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)radiusChoice).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private TableLayoutPanel tableLayout;
-        private Label cNumber;
+        private Label cNumberLabel;
         private Label iterationsLabel;
         private Label radiusLabel;
         private Label viewOffSetXLabel;
         private Label viewOffsetYLabel;
         private Label zoomLabel;
-        private TextBox сСhoice;
-        private TextBox radiusChoice;
+        private TextBox RealСhoice;
         private NumericUpDown offsetXChoice;
         private NumericUpDown offsetYChoice;
         private NumericUpDown zoomChoice;
@@ -357,5 +379,7 @@
         private Label juliaLabel;
         private Button saveButton;
         private Button renderButton;
+        private TextBox ImaginaryChoice;
+        private NumericUpDown radiusChoice;
     }
 }
--- FractalsLab1Julia6/JuliaFractal.cs
+++ FractalsLab1Julia6/JuliaFractal.cs
@@ -1,12 +1,101 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using System.Drawing.Drawing2D;
+using System.Numerics;
+using System.Windows.Forms;
+
 namespace FractalsLab1Julia6
 {
     public partial class JuliaFractalUI : Form
     {
+        private Complex complexNumber
+        {
+            get
+            {
+                return new Complex(double.Parse(RealÑhoice.Text), double.Parse(ImaginaryChoice.Text));
+            }
+            set
+            {
+                complexNumber = new Complex(double.Parse(RealÑhoice.Text), double.Parse(ImaginaryChoice.Text));
+            }
+        }
+        private int maxIterations
+        {
+            get => (int)iterationsChoice.Value;
+            set { maxIterations = (int)iterationsChoice.Value; }
+        }
+        private int radius
+        {
+            get => (int)radiusChoice.Value;
+            set { radius = (int)radiusChoice.Value; }
+        }
+        private int zoom
+        {
+            get => (int)zoomChoice.Value;
+            set { zoom = (int)zoomChoice.Value; }
+        }
+
+        private double viewOffsetX
+        {
+            get => (double)offsetXChoice.Value;
+            set { viewOffsetX = (double)offsetXChoice.Value; }
+        }
+
+        private double viewOffsetY
+        {
+            get => (double)offsetYChoice.Value;
+            set { viewOffsetY = (double)offsetYChoice.Value; }
+        }
+
+
         public JuliaFractalUI()
         {
             InitializeComponent();
         }
 
+        private void renderButton_Click(object sender, EventArgs e)
+        {
+            juliaPictureBox.Image = juliaSetBitmap();
+
+        }
+
+        private Bitmap juliaSetBitmap()
+        {
+            Complex z = new Complex(0, 0);
+            int i;
+
+            Color[] colors = (from c in Enumerable.Range(0, maxIterations + 1)
+                              select Color.FromArgb((c >> 5) * 36, (c >> 3 & 7) * 36, (c & 3) * 85)).ToArray();
+
+            var bitmap = new Bitmap(juliaPictureBox.Width, juliaPictureBox.Height);
+            for (int x = 0; x < juliaPictureBox.Width; x++)
+            {
+                for (int y = 0; y < juliaPictureBox.Height; y++)
+                {
+                    z = new Complex(1.5 * (x - juliaPictureBox.Width / 2) / (0.5 * zoom * juliaPictureBox.Width) + viewOffsetX,
+                        1.0 * (y - juliaPictureBox.Height / 2) / (0.5 * zoom * juliaPictureBox.Height) + viewOffsetY);
+                    i = 0;
+                    while (z.Magnitude < radius && i < maxIterations)
+                    {
+                        z = z * z * z * z * z;
+                        z += complexNumber;
+                        i += 1;
+                    }
+                    bitmap.SetPixel(x, y, colors[i]);
+                }
+            }
+ 
+
+
+            Graphics g = Graphics.FromImage(bitmap);
+
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            g.Flush();
+            return bitmap;
+        }
+ 
 
     }
 }
